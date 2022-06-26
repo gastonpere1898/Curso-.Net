194a252ead0edc8a08920211dbec9ffca3554ba2
Gaston Pereyra <gastonpere1898@gmail.com>
2022-06-26 20:25:32 -0300

    se saco proyecto de prueba y se agrrgo tarea de billetes

---
+++ Clase13_EJ-obligatorio/Modelo/Billetera.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase13_EJ_obligatorio.Modelo
+{
+    public class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+        public decimal ObtenerTotal()
+        {
+            return  (BilletesDe10 + BilletesDe20 + BilletesDe50 + BilletesDe100 + BilletesDe200 + BilletesDe500 + BilletesDe1000);
+
+        }
+        public Billetera Combinar(Billetera billetera)
+        {
+            var billeteraNueva = new Billetera
+            {
+                BilletesDe10 = billetera.BilletesDe10 + BilletesDe10,
+                BilletesDe20 = billetera.BilletesDe20 + BilletesDe20,
+                BilletesDe50 = billetera.BilletesDe50 + BilletesDe50,
+                BilletesDe100 = billetera.BilletesDe100 + BilletesDe100,
+                BilletesDe500 = billetera.BilletesDe500 + BilletesDe500,
+                BilletesDe1000 = billetera.BilletesDe1000 + BilletesDe1000
+            };
+
+            billetera.BilletesDe10 = 0;
+            billetera.BilletesDe20 = 0;
+            billetera.BilletesDe50 = 0;
+            billetera.BilletesDe100 = 0;
+            billetera.BilletesDe500 = 0;
+            billetera.BilletesDe1000 = 0;
+            BilletesDe10 = 0;
+            BilletesDe20 = 0;
+            BilletesDe50 = 0;
+            BilletesDe100 = 0;
+            BilletesDe500 = 0;
+            BilletesDe1000 = 0;
+            
+
+            return billeteraNueva;
+        }
+
+    }
+}
---
+++ Clase13_EJ-obligatorio/Program.cs
@@ -0,0 +1,47 @@
+﻿// Crear una clase Billetera que tenga las siguientes propiedades públicas del tipo entero:
+
+//BilletesDe10
+//BilletesDe20
+//BilletesDe50
+//BilletesDe100
+//BilletesDe200
+//BilletesDe500
+//BilletesDe1000
+
+//Agregar un método que se llame Total del tipo decimal, y que devuelva el Importe Total en la billetera
+//teniendo en cuenta la cantidad de billetes que se tenga de cada tipo.
+
+//Agregar un metodo que se llame Combinar, que reciba como parámetro otra billetera y que devuelva una nueva
+//Billetera con la suma o combinacion del dinero de ambas billeteras.  
+//Una vez combinadas las 2 billeteras (y creada la tercera), las 2 primeras billeteras deberan quedar en cero. (Sin billetes)
+using Clase13_EJ_obligatorio.Modelo;
+
+var billetera1 = new Billetera()
+{
+    BilletesDe10 = 6,
+
+};
+var billetera2 = new Billetera()
+{
+    BilletesDe10 = 2
+};
+
+
+
+
+
+Console.WriteLine("La billetera 1 tiene:");
+Console.WriteLine(billetera1.BilletesDe10 +" billetes de 10");
+
+
+Console.WriteLine("________________________________________________________");
+
+Console.WriteLine("La billetera 2 tiene:");
+Console.WriteLine(billetera2.BilletesDe10 + " billetes de 10");
+
+Console.WriteLine("________________________________________________________");
+
+var billetera3 = billetera1.Combinar(billetera2);
+
+Console.WriteLine("Entre las 2 billeteras hay:");
+Console.WriteLine(billetera3.BilletesDe10 + " billetes de 10");
